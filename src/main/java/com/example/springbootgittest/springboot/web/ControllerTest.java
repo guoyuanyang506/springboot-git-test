f5dbcc639db72b99b0f4051c1afcc7a3e2c3e425
guoyuanyang506 <a17516720639@163.com>
2020-03-07 22:33:58 +0800

    哈哈哈哈哈

---
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.springbootgittest.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+    @RequestMapping("hhh")
+    public String TestDemo() {
+        return "dsfhksdjflkdk第三方看来都是借口附近";
+    }
+
+
+}
