eee316e9e14c1d314dd072cab47fbb2c90f840b9
guoyuanyang506 <a17516720639@163.com>
2020-03-08 12:26:05 +0800

    开发人员提交

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,18 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
-    //乐山大佛金山里看得见房东说开了房间
-    //1
+
     @RequestMapping("hhh")
     public String TestDemo() {
         return "dsfhksdjflkdk第三方看来都是借口附近";
     }
-    //咖啡机多斯拉克富家大室
-    //2
-    @RequestMapping("hhh")
-    public String TestDemo1() {
-        return "dsfhksdjflkdk第三方看来都是借口附近";
-    }
-    //远程位置添加
-//立刻就过来的世界观里开放电视剧
+
 }
