9251d8b691d8bf310be080ff4a6bbc7b568a6ed7
guoyuanyang506 <a17516720639@163.com>
2020-03-08 11:53:36 +0800

    哈哈哈哈哈

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
+    //第1个方法
     @RequestMapping("hhh")
     public String TestDemo() {
-        return "dsfhksdjflkdk第三方看来都是借口附近";
+        return "dsfhksdjflkdk第三方看来都是借口附近+++++";
     }
-    //咖啡机多斯拉克富家大室
-    
-    //远程位置添加
+
+    //第二个方法
+    @RequestMapping("aaa")
+    public String TestDemo1() {
+        return "+++++";
+    }
+
 
 }
