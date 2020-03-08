2f0680b5b8145880c33b060c034d4f8c70871720
guoyuanyang506 <a17516720639@163.com>
2020-03-08 13:31:11 +0800

    开发人员提交

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,14 +5,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
+//23
+    //@RequestMapping("hhh")
 
     @RequestMapping("aaa")
     public String TestDemo() {
         //的规范多斯拉克建档立卡
-        return "dsfhksdjflkdk第三方看来都是借口附近
-            lfgfdskgj";
+        return "dsfhksdjflkdk第三方看来都是借口附近";
+            //lfgfdskgj";
         
     }
-
+//1
+    @RequestMapping("hhh")
+    public String TestDemo1() {
+        return "dsfhksdjflkdk第三方看来都是借口附近";
+        //还都是风景
+    }
 
 }
