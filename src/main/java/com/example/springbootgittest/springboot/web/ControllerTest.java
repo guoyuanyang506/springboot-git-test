a1bc272e1212b1673eed564718d22747b952e0c3
guoyuanyang506 <a17516720639@163.com>
2020-03-08 11:56:44 +0800

    哈哈哈哈哈

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
+    //第1个方法
     @RequestMapping("hhh")
     public String TestDemo() {
-        return "dsfhksdjflkdk第三方看来都是借口附近";
+        return "dsfhksdjflkdk第三方看来都是借口附近+++++";
     }
-    //咖啡机多斯拉克富家大室
-    
-    //远程位置添加
-    //梵蒂冈很多时间发回来的伤口恢复来看打扫房间Ldsfhdskfjdksf都是废话了第三方京东数科解放东路开始放假
+
+    //第二个方法
+    @RequestMapping("aaa")
+    public String TestDemo1() {
+        return "+++++";
+    }
+
 
 }
