760bfc3e4880a9972e59c97b52c0f71d8f4059b9
gyy <61364526+guoyuanyang506@users.noreply.github.com>
2020-03-08 11:58:05 +0800

    Update ControllerTest.java

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,17 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
-    //第1个方法
+    //乐山大佛金山里看得见房东说开了房间
     @RequestMapping("hhh")
     public String TestDemo() {
         return "dsfhksdjflkdk第三方看来都是借口附近+++++";
     }
 
-    //第二个方法
-    @RequestMapping("aaa")
-    public String TestDemo1() {
-        return "+++++";
-    }
+    //立刻就过来的世界观里开放电视剧
 
 
 }
