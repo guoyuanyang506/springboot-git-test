53f39c3dfaf841f60a8b286b325ca4cffcd9ad53
guoyuanyang506 <a17516720639@163.com>
2020-03-08 13:26:06 +0800

    开发人员提交

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
-
+//2
     @RequestMapping("hhh")
     public String TestDemo() {
         return "dsfhksdjflkdk第三方看来都是借口附近";
         
+    }
+//1
+    @RequestMapping("hhh")
+    public String TestDemo1() {
+        return "dsfhksdjflkdk第三方看来都是借口附近";
+        //还都是风景
     }
 
 }
