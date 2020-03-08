b35ea7c1701f7512827d1d469dc95038ecd0bdcb
gyy <61364526+guoyuanyang506@users.noreply.github.com>
2020-03-08 13:27:51 +0800

    Update ControllerTest.java

--- src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
+++ src/main/java/com/example/springbootgittest/springboot/web/ControllerTest.java
@@ -5,17 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
-//2
-    @RequestMapping("hhh")
+
+    @RequestMapping("aaa")
     public String TestDemo() {
-        return "dsfhksdjflkdk第三方看来都是借口附近";
+        //的规范多斯拉克建档立卡
+        return "dsfhksdjflkdk第三方看来都是借口附近
+            lfgfdskgj";
         
     }
-//1
-    @RequestMapping("hhh")
-    public String TestDemo1() {
-        return "dsfhksdjflkdk第三方看来都是借口附近";
-        //还都是风景
-    }
+
 
 }
